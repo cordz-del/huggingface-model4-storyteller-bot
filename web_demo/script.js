01fe3d4f947c4b0196641fad9e7280d8c187626c
cordz-del <raarongraham@gmail.com>
2025-04-11 13:21:09 -0700

    Add functionality to script.js

--- web_demo/script.js
+++ web_demo/script.js
@@ -1,1 +1,19 @@
 // This file will contain the JavaScript for the web demo
+document.getElementById('submitBtn').addEventListener('click', function() {
+    const userInput = document.getElementById('userInput').value;
+    fetch('https://api-inference.huggingface.co/models/raarongraham/storyteller-bot', {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer YOUR_HF_API_TOKEN',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ inputs: userInput })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('output').innerText = data[0].generated_text;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
